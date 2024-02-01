08fe2d71e7550e9d99f8275d0aa696f51b75dad4
Noam Anisfeld <NoamAnisfeld@gmail.com>
2024-02-01 20:07:36 +0200

    a popup allowing selection of an HTML file

--- src/pages/popup/Popup.tsx
+++ src/pages/popup/Popup.tsx
@@ -1,13 +1,53 @@
-import React from 'react';
-import logo from '@assets/img/logo.svg';
+import React, { useState } from 'react';
 import '@pages/popup/Popup.css';
 import useStorage from '@src/shared/hooks/useStorage';
 import exampleThemeStorage from '@src/shared/storages/exampleThemeStorage';
 import withSuspense from '@src/shared/hoc/withSuspense';
 import withErrorBoundary from '@src/shared/hoc/withErrorBoundary';
 
+
+const ACCEPTED_FORMAT = 'text/html';
+
+async function readFile(file: File) {
+  return new Promise(resolve => {
+    const reader = new FileReader();
+    reader.addEventListener("load", () => resolve(reader.result));
+    reader.readAsText(file);
+  });
+}
+
+async function injectFileContent(file: File) {
+  if (!file) {
+    return;
+  }
+
+  const fileContent = await readFile(file);
+  alert(fileContent);
+}
+
 const Popup = () => {
+
   const theme = useStorage(exampleThemeStorage);
+  const [file, setFile] = useState<File>();
+  const [errorMessage, setErrorMessage] = useState('');
+
+  function handleFileSelected(e: React.ChangeEvent<HTMLInputElement>) {
+    setErrorMessage('');
+
+    const inputFile = e.target.files[0];
+    if (inputFile && inputFile.type === ACCEPTED_FORMAT) {
+      setFile(inputFile);
+    }
+  }
+
+  function handleSubmit() {
+    if (!file) {
+      setErrorMessage('שגיאה: לא נבחר קובץ');
+    } else {
+      setErrorMessage('');
+      injectFileContent(file);
+    }
+  }
 
   return (
     <div
@@ -15,27 +55,16 @@ const Popup = () => {
       style={{
         backgroundColor: theme === 'light' ? '#fff' : '#000',
       }}>
+
       <header className="App-header" style={{ color: theme === 'light' ? '#000' : '#fff' }}>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/pages/popup/Popup.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-          style={{ color: theme === 'light' && '#0281dc', marginBottom: '10px' }}>
-          Learn React!
-        </a>
-        <button
-          style={{
-            backgroundColor: theme === 'light' ? '#fff' : '#000',
-            color: theme === 'light' ? '#000' : '#fff',
-          }}
-          onClick={exampleThemeStorage.toggle}>
-          Toggle theme
-        </button>
+        <label>
+          <input type="file" accept={ACCEPTED_FORMAT} onChange={handleFileSelected} />
+        </label>
+        <button onClick={handleSubmit}>השתמש בקובץ</button>
+        {errorMessage
+          ? <p className="error">{errorMessage}</p>
+          : undefined
+        }
       </header>
     </div>
   );
